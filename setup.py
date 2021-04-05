cfef96c3aa0a242595ac8dcb2be5f0f9c594b0f3
Yang Luo <hsluoyz@qq.com>
2021-04-05 11:54:06 +0800

    Update pkg name.

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r") as readme_file:
 requirements = ["casbin==0.8.4", "psycopg2-binary==2.8.6", "black==20.8b1"]
 
 setup(
-    name="postgresql-watcher",
+    name="casbin-postgresql-watcher",
     version="0.0.1",
     author="hsluoyz",
     author_email="hsluoyz@gmail.com",
-    description="Casbin role watcher to be used for monitoring updates to casbin policies for PyCasbin",
+    description="Casbin role watcher to be used for monitoring updates to policies for PyCasbin",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/pycasbin/postgresql-watcher",
