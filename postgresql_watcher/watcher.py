90689ef151a4cc796bb6f6061fc91b9c61d30621
Thore Bartholomaeus <thore.bar@gmail.com>
2024-07-08 15:19:11 +0200

    chore: Some code reorg        - Make PostgresqlWatcher.create_subscription_process a private method        - Rename casbin_subscription to _casbin_channel_subscription

--- postgresql_watcher/watcher.py
+++ postgresql_watcher/watcher.py
@@ -10,49 +10,6 @@ from logging import Logger, getLogger
 POSTGRESQL_CHANNEL_NAME = "casbin_role_watcher"
 
 
-def casbin_subscription(
-    process_conn: Connection,
-    logger: Logger,
-    host: str,
-    user: str,
-    password: str,
-    port: Optional[int] = 5432,
-    dbname: Optional[str] = "postgres",
-    delay: Optional[int] = 2,
-    channel_name: Optional[str] = POSTGRESQL_CHANNEL_NAME,
-    sslmode: Optional[str] = None,
-    sslrootcert: Optional[str] = None,
-    sslcert: Optional[str] = None,
-    sslkey: Optional[str] = None,
-):
-    # delay connecting to postgresql (postgresql connection failure)
-    time.sleep(delay)
-    conn = connect(
-        host=host,
-        port=port,
-        user=user,
-        password=password,
-        dbname=dbname,
-        sslmode=sslmode,
-        sslrootcert=sslrootcert,
-        sslcert=sslcert,
-        sslkey=sslkey,
-    )
-    # Can only receive notifications when not in transaction, set this for easier usage
-    conn.set_isolation_level(extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-    curs = conn.cursor()
-    curs.execute(f"LISTEN {channel_name};")
-    logger.debug("Waiting for casbin policy update")
-    while True and not curs.closed:
-        if not select([conn], [], [], 5) == ([], [], []):
-            logger.debug("Casbin policy update identified..")
-            conn.poll()
-            while conn.notifies:
-                notify = conn.notifies.pop(0)
-                logger.debug(f"Notify: {notify.payload}")
-                process_conn.send(notify.payload)
-
-
 class PostgresqlWatcher(object):
 
     @staticmethod
@@ -99,9 +56,9 @@ class PostgresqlWatcher(object):
         self.parent_conn: Connection = None
         self.child_conn: Connection = None
         self.subscription_process: Process = None
-        self.create_subscription_process(start_process)
+        self._create_subscription_process(start_process)
 
-    def create_subscription_process(
+    def _create_subscription_process(
         self,
         start_process: Optional[bool] = True,
         delay: Optional[int] = 2,
@@ -119,7 +76,7 @@ class PostgresqlWatcher(object):
 
         self.parent_conn, self.child_conn = Pipe()
         self.subscribed_process = Process(
-            target=casbin_subscription,
+            target=_casbin_channel_subscription,
             args=(
                 self.child_conn,
                 self.logger,
@@ -145,6 +102,7 @@ class PostgresqlWatcher(object):
         self.update_callback = fn_name
 
     def update(self):
+
         conn = connect(
             host=self.host,
             port=self.port,
@@ -176,6 +134,49 @@ class PostgresqlWatcher(object):
                 "Child casbin-watcher subscribe process has stopped, "
                 "attempting to recreate the process in 10 seconds..."
             )
-            self.create_subscription_process(delay=10)
+            self._create_subscription_process(delay=10)
 
         return False
+
+
+def _casbin_channel_subscription(
+    process_conn: Connection,
+    logger: Logger,
+    host: str,
+    user: str,
+    password: str,
+    port: Optional[int] = 5432,
+    dbname: Optional[str] = "postgres",
+    delay: Optional[int] = 2,
+    channel_name: Optional[str] = POSTGRESQL_CHANNEL_NAME,
+    sslmode: Optional[str] = None,
+    sslrootcert: Optional[str] = None,
+    sslcert: Optional[str] = None,
+    sslkey: Optional[str] = None,
+):
+    # delay connecting to postgresql (postgresql connection failure)
+    time.sleep(delay)
+    conn = connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        dbname=dbname,
+        sslmode=sslmode,
+        sslrootcert=sslrootcert,
+        sslcert=sslcert,
+        sslkey=sslkey,
+    )
+    # Can only receive notifications when not in transaction, set this for easier usage
+    conn.set_isolation_level(extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    curs = conn.cursor()
+    curs.execute(f"LISTEN {channel_name};")
+    logger.debug("Waiting for casbin policy update")
+    while True and not curs.closed:
+        if not select([conn], [], [], 5) == ([], [], []):
+            logger.debug("Casbin policy update identified..")
+            conn.poll()
+            while conn.notifies:
+                notify = conn.notifies.pop(0)
+                logger.debug(f"Notify: {notify.payload}")
+                process_conn.send(notify.payload)
